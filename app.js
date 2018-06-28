5bc668758dd41520ec20d9f2b3f7962764e5cf19
Richard Astbury <richard.astbury@gmail.com>
2018-06-28 13:50:18 +0100

    add localtunnel

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 var youtubeDl = require('./lib/youtubedl');
 var search = require('./lib/youtubeSearch');
 var player = require('./lib/player');
+var localtunnel = require('localtunnel');
 
 var playQueue = [];
 var playing = false;
+
+
+var settings = require('./client_secret.json');
+if (settings.subdomain){
+    localtunnel(8080, { subdomain:settings.subdomain }, (err,tunnel) => {
+        if (err) return console.log("error", err);
+        console.log(tunnel);
+        tunnel.on("close", () => console.log("TUNNEL CLOSED") );
+        tunnel.on("error", err => console.log("TUNNEL ERROR", err) );
+    });
+}
 
 async function enqueue(term){
     var searchResult = await search(term);
